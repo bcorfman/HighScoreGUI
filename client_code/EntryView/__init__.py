b48c124ab4b2edb15aef404c6c79d25cd3b7cffe
bcorfman1@gmail.com <bcorfman1@gmail.com>
2023-10-17 18:26:36 +0000

    Removing table refs and imports

--- client_code/EntryView/__init__.py
+++ client_code/EntryView/__init__.py
@@ -1,9 +1,6 @@
 from ._anvil_designer import EntryViewTemplate
 from anvil import *
 import anvil.server
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 from ..EntryEdit import EntryEdit
 
 class EntryView(EntryViewTemplate):
