5541a084ea66d88094e9ea59da2d2bc8ffb31616
bcorfman1@gmail.com <bcorfman1@gmail.com>
2023-10-17 17:29:01 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,37 +1,19 @@
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
+import anvil.http
 import anvil.server
-from datetime import datetime
 
-
-@anvil.server.callable
-def add_entry(entry_dict):
-  app_tables.entries.add_row(
-    created=datetime.now(),
-    **entry_dict
-  )
+HIGHSCORE_SERVICE = "https://highscore-ibq0itxr.b4a.run"
 
 @anvil.server.callable
-def get_entries():
-  # Get a list of entries from the Data Table, sorted by 'created' column, in descending order
-  return app_tables.entries.search(
-    tables.order_by("created", ascending=False)
-  )
+def add_score(initials, score):
+  result = anvil.http.request(HIGHSCORE_SERVICE + f'/add_score?initials={initials}&score={score}', 'POST')
+  return result.json()
 
 @anvil.server.callable
-def update_entry(entry, entry_dict):
-  # check that the entry given is really a row in the ‘entries’ table
-  if app_tables.entries.has_row(entry):
-    entry_dict['updated'] = datetime.now()
-    entry.update(**entry_dict)
-  else:
-    raise Exception("Entry does not exist")
-
+def get_high_scores():
+  result = anvil.http.request(HIGHSCORE_SERVICE + '/high_scores', 'GET')
+  return result.json()
+  
 @anvil.server.callable
-def delete_entry(entry):
-  # check that the entry being deleted exists in the Data Table
-  if app_tables.entries.has_row(entry):
-    entry.delete()
-  else:
-    raise Exception("Entry does not exist")
+def clear_high_scores():
+  result = anvil.http.request(HIGHSCORE_SERVICE + '/clear_scores', 'POST')
+  return result.json()
